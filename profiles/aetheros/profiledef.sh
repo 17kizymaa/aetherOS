f52574c8328c36057b1cfc4f8630ddf25820740f
anphuni <17kizymaa@github.com>
2026-06-03 22:55:05 +0100

    feat(01-02): create mkarchiso profile aetheros
    - Create profiledef.sh with ISO metadata (aetheros, v0.1.0-demo.1, x86_64)
    - Create packages.x86_64 with minimal Arch + XFCE system (13 packages)
    - Create pacman.conf with core/extra/multilib repos
    - Create boot config directories (syslinux, grub, efiboot)
    
    Co-Authored-By: OWL <noreply@openrouter.xyz>

---
+++ profiles/aetheros/profiledef.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# profiles/aetheros/profiledef.sh
+
+iso_name="aetheros"
+iso_label="AETHEROS_01"
+iso_publisher="aetherOS Project <https://github.com/anphuni/aetherOS>"
+iso_application="aetherOS Live/Rescue CD"
+iso_version="0.1.0-demo.1"
+install_dir="aetheros"
+buildmodes=("iso")
+bootmodes=("bios.syslinux" "uefi.grub")
+arch="x86_64"
